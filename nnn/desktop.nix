92bbebc8bee423ac33e7268900c6a3c8836cab2e
BetaD46 <1057403878@qq.com>
2026-05-08 21:36:13 +0800

    add regreet

--- nnn/desktop.nix
+++ nnn/desktop.nix
@@ -29,4 +29,33 @@
 
   programs.firefox.enable = true;
   programs.niri.enable = true;
+
+  programs.regreet = {
+    enable = true;
+      settings = {
+        GTK.application_prefer_dark_theme = true;
+      };
+      # For this example you'd need to have a version of Adwaita and the font Cantarell installed
+      theme.name = "Adwaita"; 
+      font = {
+        name = "Cantarell";
+        size = 16;
+      };
+      cursorTheme.name = "Adwaita";
+  };
+
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${config.programs.niri.package}/bin/niri-session";
+  #       user = "wjf";
+  #     };
+  #   };
+  # };
+
+  # NixOS otherwise injects a stripped PATH via Environment= on the niri.service
+  # unit which shadows the imported user-manager PATH. Disabling the default
+  # lets niri inherit the full PATH set up by niri-session.
+  # systemd.user.services.niri.enableDefaultPath = false;
 }
